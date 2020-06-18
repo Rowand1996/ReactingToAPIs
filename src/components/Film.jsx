f447704459be6cb76b0565fae7e10a619dde7410
Rowan Douglas <rowanandvera@gmail.com>
2020-06-17 20:23:37 -0500

    changed qoute color to black

--- src/components/Film.jsx
+++ src/components/Film.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Film = (props) => {
     let filmItems = props.items.map((film) => {
         return (
-            <div key={film.id} className="card">
+            <div key={film.id} className="card bg-info ">
                 <div className="card-header">
                     Movie Title: {film.title}
                 </div>
                 <div className="card-body">
                     <blockquote className="blockquote mb-0">
                         <p>{film.description}</p>
-                        <footer className="blockquote-footer">Movie Director: {film.director}<cite title="Source Title"></cite></footer>
+                        <footer className="blockquote-footer text-dark">Movie Director: {film.director}<cite title="Source Title"></cite></footer>
                     </blockquote>
                 </div>
             </div>
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Person = (props) => {
     let moviePeople = props.items.map((person) => {
         return (
-            <div key={person.id} className="card">
+            <div key={person.id} className="card bg-info">
                 <div className="card-header">
                     Person Name: {person.name}
                 </div>
                 <div className="card-body">
                     <blockquote className="blockquote mb-0">
                         <p>Gender: {person.gender}</p>
-                        <footer className="blockquote-footer">Person Age: {person.age}<cite title="Source Title"></cite></footer>
+                        <footer className="blockquote-footer text-dark">Person Age: {person.age}<cite title="Source Title"></cite></footer>
                     </blockquote>
                 </div>
             </div>
