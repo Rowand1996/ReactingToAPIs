4123a3f43b298715cbf3ac95285efb95cbd8c35f
Rowan Douglas <rowanandvera@gmail.com>
2020-06-17 20:38:11 -0500

    displayed properties of person in another tab

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Person = (props) => {
     let moviePeople = props.items.map((person) => {
+        let idLink = "https://ghibliapi.herokuapp.com/people/" + person.id;
         return (
             <div key={person.id} className="card bg-info">
                 <div className="card-header">
-                    Person Name: {person.name}
+                    Person Name: <a rel="noopener noreferrer" target="_blank" href={idLink}>{person.name}</a>
                 </div>
                 <div className="card-body">
                     <blockquote className="blockquote mb-0">
